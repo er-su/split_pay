e45a4bd8f1420f07f33ffa3b01035a4a67b31f80
waffleviet <56283029+waffleviet@users.noreply.github.com>
2025-10-29 17:40:41 -0700

    Update groupTrans.tsx

---
+++ frontend/src/group/groupTrans.tsx
@@ -0,0 +1,57 @@
+import './App.css'
+import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import api from '../components/api';
+import GoogleLogin from '../components/googleLogin';
+import { useEffect } from 'react';
+
+
+function groupTrans() {
+const navigate = useNavigate();
+const AddGroupForm = () => {
+     navigate("/Groupform");
+   };
+
+  const [groups, Setgroups] = useState([]);
+  const Getgroups = async () => {
+    try {
+      const res = await api.get('/me/groups');
+      console.log("User data:", res.data);
+      Setgroups(res.data);
+    } 
+    catch (error: any) 
+    {
+    console.error("User not authenticated");
+    GoogleLogin()
+    return null;
+    }
+    };
+  useEffect(() => {
+    Getgroups();
+  }, []);
+
+   return (
+    <div style={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}>
+      {groups.map((group: any) => (
+        <div 
+          key={group.id} 
+          style={{
+            border: "1px solid #ccc",
+            padding: "10px",
+            borderRadius: "8px",
+            width: "200px",
+            backgroundColor: "#f9f9f9",
+          }}
+        >
+          <h3>{group.name} {group.base_currency}</h3>
+        </div>
+      ))}
+      <button onClick={AddGroupForm}>Create A group</button>
+    </div>
+    
+  );
+ 
+ 
+}
+
+export default groupTrans
